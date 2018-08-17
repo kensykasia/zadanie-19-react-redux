fb6ee5bfd06c4dc4e75098c566194d92ff6547b7
Katarzyna Kensy <zbierek@gmail.com>
2018-08-17 18:55:33 +0200

    Add all files

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import DevTools from './DevTools';
+import reducer from './reducer';
+import { addComment } from './actions';
+
+const store = createStore(
+  reducer,
+  DevTools.instrument()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+store.dispatch(addComment('pierwszy komentarz'));
+store.dispatch(addComment('drugi komentarz'));
+store.dispatch(addComment('trzeci komentarz'));
+
+registerServiceWorker();
